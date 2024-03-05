b33d668d9bb996fdf0477831e0bc5b3bdfeca7fd
Pawel <pbroniarczyk@outlook.com>
2024-03-05 16:58:43 +0100

    removed react and next

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,14 +6,6 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: "module",
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
-  settings: {
-    react: {
-      version: "detect",
-    },
   },
   extends: [
     "plugin:react/recommended",
