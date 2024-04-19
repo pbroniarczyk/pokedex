3a5fbb2ad1cfadc6c8a00e75d3df2a864f0792dc
Pawel <pbroniarczyk@outlook.com>
2024-04-19 10:19:36 +0200

    Added Pokemon types

---
+++ client/src/core/pokemon-type.ts
@@ -0,0 +1,41 @@
+export type PokemonType = [
+  | "bug"
+  | "dark"
+  | "dragon"
+  | "electric"
+  | "fairy"
+  | "fighting"
+  | "fire"
+  | "flying"
+  | "ghost"
+  | "normal"
+  | "grass"
+  | "ground"
+  | "ice"
+  | "poison"
+  | "psychic"
+  | "rock"
+  | "steel"
+  | "water",
+];
+
+export type TypeColorVariants = {
+  bug: string;
+  dark: string;
+  dragon: string;
+  electric: string;
+  fairy: string;
+  fighting: string;
+  fire: string;
+  flying: string;
+  ghost: string;
+  normal: string;
+  grass: string;
+  ground: string;
+  ice: string;
+  poison: string;
+  psychic: string;
+  rock: string;
+  steel: string;
+  water: string;
+};
